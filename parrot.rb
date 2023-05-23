78bbbda6f8c091f47422049c447824b3f4b65421
keruboelizabeth02 <keruboelizabeth02@gmail.com>
2023-05-23 14:49:06 +0300

    completd

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,13 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot
+    puts "Squawk!"
+end
+
+def parrot(name = "Squawk!")
+    puts "#{name}"
+    "#{name}"
+end
+
+parrot("Pretty bird!")
